5891480f84e6327e7011f8d65bcd3225976eaeca
Elk87 <enriquegar2003@gmail.com>
2024-02-23 11:44:03 +0100

    Initial commit

---
+++ src/main/java/com/example/gymweb/GymWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.gymweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymWebApplication.class, args);
+    }
+
+}
