cd991118c296eefe04d7f4edfcce9d9df1f4bc7a
erikarusa <161024556+erikarusa@users.noreply.github.com>
2024-04-10 23:13:40 +0200

    repositories and services

--- src/main/java/com/example/gymweb/Services/RankingService.java
+++ src/main/java/com/example/gymweb/Services/RankingService.java
@@ -8,7 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.concurrent.atomic.AtomicLong;
+//import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class RankingService {
