80d46fdba7f676641ee753d1962e1c51db81ccc4
erikarusa <161024556+erikarusa@users.noreply.github.com>
2024-05-03 17:49:38 +0200

    add jwt tokens

--- src/main/java/com/example/gymweb/Secure/WebSecurityConfig.java
+++ src/main/java/com/example/gymweb/Secure/WebSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.example.gymweb.Secure;
 
+import com.example.gymweb.Secure.jwt.JwtRequestFilter;
+import com.example.gymweb.Secure.jwt.UnauthorizedHandlerJwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -15,10 +20,19 @@ import org.springframework.security.web.SecurityFilterChain;
 public class WebSecurityConfig {
     @Autowired
     public RepositoryUserDetailsService userDetailsService;
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration authenticationConfig) throws Exception {
+        return authenticationConfig.getAuthenticationManager();
+    }
+
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Bean
     DaoAuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
@@ -28,12 +42,13 @@ public class WebSecurityConfig {
     }
 
     @Bean
+    @Order(2)
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authenticationProvider(authenticationProvider());
         http
-                .authorizeHttpRequests(authorize ->authorize
+                .authorizeHttpRequests(authorize -> authorize
                         //static resources
-                        .requestMatchers("/css/**","/js/**","/img/**","/bootstrap/**").permitAll()
+                        .requestMatchers("/css/**", "/js/**", "/img/**", "/bootstrap/**").permitAll()
                         //public pages
                         .requestMatchers("/").permitAll()
                         .requestMatchers("/login").permitAll()
@@ -50,12 +65,26 @@ public class WebSecurityConfig {
                         .permitAll())
 
                 //logout
-                .logout(logout->logout
+                .logout(logout -> logout
                         .logoutUrl("/logout")
                         .logoutSuccessUrl("/")
                         .permitAll()); //punto y coma cierra todo*/
 
         return http.build();
     }
-
 }
+
+   /* @Bean
+    @Order(1)
+    public SecurityFilterChain apiFilterChain(HttpSecurity http, UnauthorizedHandlerJwt unauthorizedHandlerJwt) throws Exception {
+        http.authenticationProvider(authenticationProvider());
+        http
+                .securityMatcher("/api/**")
+                .exceptionHandling(handling -> handling.authenticationEntryPoint(unauthorizedHandlerJwt));
+        /*http
+                .authorizeHttpRequests(authorize -> authorize
+
+
+                );
+    }
+}*/
