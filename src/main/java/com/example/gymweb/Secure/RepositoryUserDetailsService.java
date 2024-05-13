e84df6060dabaed045318847350b4d22455a79e2
Elk87 <124152817+Elk87@users.noreply.github.com>
2024-05-13 19:31:26 +0200

    excepcion user not found

--- src/main/java/com/example/gymweb/Secure/RepositoryUserDetailsService.java
+++ src/main/java/com/example/gymweb/Secure/RepositoryUserDetailsService.java
@@ -21,6 +21,9 @@ public class RepositoryUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
         List<GrantedAuthority> roles = new ArrayList<>();
         for (String role : user.getRoles()) {
             roles.add(new SimpleGrantedAuthority("ROLE_" + role));
